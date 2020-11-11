65abe56af9d1b084ebc41a49e2cb416236e4eada
Andrew <acasta4704@gmail.com>
2020-11-11 14:31:31 +1100

    add base home & about pages with nav, sidebar & cart overlays

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,1 +1,12 @@
-import { getElement } from './utils.js';
+import { getElement } from "./utils.js";
+
+const sideBar = getElement(".sidebar-overlay");
+const toggleNav = getElement(".toggle-nav");
+const closeNav = getElement(".sidebar-close");
+
+toggleNav.addEventListener("click", () => {
+  sideBar.classList.add("show");
+});
+closeNav.addEventListener("click", () => {
+  sideBar.classList.remove("show");
+});
